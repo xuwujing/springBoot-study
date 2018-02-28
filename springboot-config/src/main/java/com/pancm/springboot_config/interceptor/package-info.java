610071fad79c63617a44da7e55d798442548b0cf
xuwujing <1060589146@qq.com>
2018-02-28 17:01:42 +0800

    1.自定义配置获取和过滤器以及拦截器使用

---
+++ springboot-config/src/main/java/com/pancm/springboot_config/interceptor/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+/**
+ * Title: package-info
+ * Description: 
+ * 拦截器
+ * Version:1.0.0  
+ * @author pancm
+ * @date 2018年2月28日
+ */
+package com.pancm.springboot_config.interceptor;
---
+++ springboot-config/src/main/java/com/pancm/springboot_config/web/HelloWorldController.java
@@ -0,0 +1,45 @@
+package com.pancm.springboot_config.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.pancm.springboot_config.bean.User;
+import com.pancm.springboot_config.config.MyProperties;
+
+/**
+ * 
+* Title: HelloWorldController
+* Description: springboot 接口测试
+* 首先启动 Application 程序，然后在浏览器输入http://localhost:8080//hello 
+* 即可查看信息
+* 在类中添加  @RestController, 默认类中的方法都会以json的格式返回
+* Version:1.0.0  
+* @author pancm
+* @date 2018年1月3日
+ */
+@RestController
+public class HelloWorldController {
+	@Autowired
+	MyProperties myProperties;
+	
+    @RequestMapping("/hello")
+    public String index() {
+    	System.out.println("---------开始----------");
+    	System.out.println("title:"+myProperties.getTitle());
+    	System.out.println("describe:"+myProperties.getDescription());
+        return "Hello World";
+    }
+    
+     
+    @RequestMapping("/getUser")
+    public User getUser() {
+    	System.out.println("---------开始----------");
+    	User user=new User();
+    	user.setId(2);
+    	user.setName("李四");
+        return user;
+    }
+    
+    
+}
