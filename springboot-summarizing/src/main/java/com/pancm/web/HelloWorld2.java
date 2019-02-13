3b729b6fa12bdb8da3086bec1ea86a784d9e95c2
xuwujing <1060589146@qq.com>
2019-02-13 17:55:08 +0800

    shiro 代码提交

--- springboot-summarizing/src/main/java/com/pancm/web/HelloWorld2.java
+++ springboot-summarizing/src/main/java/com/pancm/web/HelloWorld2.java
@@ -9,21 +9,21 @@ import com.pancm.pojo.User;
 
 /**
  * 
-* Title: HelloWorld2
-* Description: 
-* @EnableAutoConfiguration 用于自动配置。
+* @Title: HelloWorld2
+* @Description: 
+* EnableAutoConfiguration 用于自动配置。
 * 简单的说，它会根据你的pom配置（实际上应该是根据具体的依赖）来判断这是一个什么应用，并创建相应的环境。
-* Version:1.0.0  
+* @Version:1.0.0  
 * @author pancm
 * @date 2018年1月8日
  */
-
 @Controller
 @EnableAutoConfiguration
 public class HelloWorld2 {
 	 
 	   
-		@RequestMapping("/2")
+		//这种写法表示两个地址可以访问
+		@RequestMapping({"/1","/2"})
 	    @ResponseBody
 	 	 public User getUser() {
 	     	// 直接获取界面会直接返回 {"id":2,"name":"李四"}
