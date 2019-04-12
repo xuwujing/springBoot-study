4292576bc90e359204c92479e58c3ebb8a151ab0
xuwujing <1060589146@qq.com>
2019-04-12 10:19:44 +0800

    1.springboot2.x工程增加注释说明

--- springboot-2/src/main/java/com/pancm/handler/HelloHandler.java
+++ springboot-2/src/main/java/com/pancm/handler/HelloHandler.java
@@ -1,4 +1,4 @@
-package com.pancm.web;
+package com.pancm.handler;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
--- springboot-2/src/main/java/com/pancm/web/HelloRouter.java
+++ springboot-2/src/main/java/com/pancm/web/HelloRouter.java
@@ -1,5 +1,6 @@
 package com.pancm.web;
 
+import com.pancm.handler.HelloHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
@@ -13,6 +14,10 @@ import org.springframework.web.reactive.function.server.ServerResponse;
  * @Title: HelloRouter
  * @Description:请求路由处理类
  主要 提供接口调用，类似  @RequestMapping 注解，可以理解为mvc中的controller
+启动程序之后,在浏览器输入:http://localhost:8280/webflux/hello 即可查看返回信息
+在浏览器输入:http://localhost:8280/actuator 即可查看程序的相关信息
+
+springboot2.x的配置说明:https://github.com/spring-projects/spring-boot/wiki/Spring-Boot-2.0-Migration-Guide
  * @Version:1.0.0
  * @Since: jdk1.8
  * @author pancm
