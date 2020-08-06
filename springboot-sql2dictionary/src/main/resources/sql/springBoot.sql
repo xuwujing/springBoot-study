87cf5b8f95638d9531facb2be11cd2919a58ef8b
xuwujing <1060589146@qq.com>
2020-08-06 10:24:08 +0800

    添加sql语句

---
+++ springboot-sql2dictionary/src/main/resources/sql/springBoot.sql
@@ -0,0 +1,21 @@
+/*
+SQLyog Ultimate v12.09 (64 bit)
+MySQL - 5.5.27 : Database - springboot
+*********************************************************************
+*/
+CREATE DATABASE `springboot`;
+
+USE `springboot`;
+
+/*Table structure for table `t_user` */
+
+DROP TABLE IF EXISTS `t_user`;
+
+CREATE TABLE `t_user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `name` varchar(10) DEFAULT NULL COMMENT '姓名',
+  `age` int(2) DEFAULT NULL COMMENT '年龄',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
+
+
